02b0c39d8e0ec892135a3d4787557f34773b382e
perlancar (on pc-office) <perlancar@gmail.com>
2017-11-10 16:51:38 +0700

    WIP

---
+++ lib/URI/Parse/SearchString/Plugin/Google.pm
@@ -0,0 +1,15 @@
+package URI::Parse::SearchString::Plugin::Google;
+
+# DATE
+# VERSION
+
+use strict;
+use warnings;
+
+use parent 'URI::Parse::SearchString::Plugin';
+
+our %HOSTS = (
+);
+
+1;
+# ABSTRACT: Various Google sites
